d13fd8776a61eb7ac06c6d04d16f1b4795f12b04
Crowdbotics <team@crowdbotics.com>
2021-11-27 02:58:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cariplay_32170",
+  url: "https://cariplay_32170.botics.co",
+  api: "https://cariplay_32170.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
